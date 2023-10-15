1394963c0981054dffb50d9e88d0b7d5e83ccf7a
Łukasz Rodak <lukasz.rodak89@gmail.com>
2023-10-16 01:59:20 +0200

    Stworzenie pustej aplikacji

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var myAge = 30;
+var sex = "kobieta";
+
+
+if (name == "Ewa" && myAge == 30 && sex == "kobieta")
+{
+    Console.WriteLine("Ewa, 30 lat");
+}
+else if (name != "Ewa" && myAge == 30 && sex == "kobieta")
+{
+    Console.WriteLine("kobieta, 30 lat");
+}
+else if (myAge < 30 && sex == "kobieta")
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (myAge < 30 && sex == "mężczyzna")
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+else if (myAge < 30 && sex == "mężczyzna")
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("z poza badanej grupy");
+}
