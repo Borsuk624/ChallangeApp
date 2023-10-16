797818cfc4a5e0618ca79b0b5f52158ce77a699d
Łukasz Rodak <lukasz.rodak89@gmail.com>
2023-10-16 02:31:36 +0200

    Stworzenie pustej aplikacji

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,52 +1,24 @@
 ﻿string name = "Ewa";
-int myAge = 30;
+int age = 30;
 string sex = "kobieta";
-
-if (name != "Ewa" && myAge < 30 && sex == "kobieta")
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else
-{
-    Console.WriteLine("z poza badanej grupy");
-}
-
-if (name == "Ewa" && myAge == 30 && sex == "kobieta")
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else
-{
-    Console.WriteLine("z poza badanej grupy");
-}
-
-if (name != "Ewa" && myAge < 18 && sex != "kobieta")
-{
-    Console.WriteLine("niepełnoletni mężczyna");
-}
-else
-{
-    Console.WriteLine("z poza badanej grupy");
-}
-
 
 // połączone
 
-if (name == "Ewa" && myAge == 30 && sex == "kobieta")
+if (name == "Ewa" && age == 30 && sex == "kobieta")
 {
     Console.WriteLine("Ewa, 30 lat");
 }
-else if (name != "Ewa" && myAge == 30 && sex == "kobieta")
+else if (name != "Ewa" && age == 30 && sex == "kobieta")
 {
     Console.WriteLine("kobieta, 30 lat");
 }
-else if (myAge < 30 && sex == "kobieta")
+else if (age < 30 && sex == "kobieta")
 {
     Console.WriteLine("kobieta poniżej 30 lat");
 }
-else if (myAge < 30 && sex != "kobieta")
+else if (age < 30 && sex != "kobieta")
 {
-    if (myAge < 18 && sex != "kobieta")
+    if (age < 18 && sex != "kobieta")
     {
         Console.WriteLine("niepełnoletni mężczyzna");
     }
