ab2365c7343e7664b2d163fe62303a3a516f14df
Łukasz Rodak <lukasz.rodak89@gmail.com>
2023-11-05 12:31:46 +0100

    Dzień 7 Test

---
+++ ChallengeApp/ChallengeApp.Test/employee.Test.cs
@@ -0,0 +1,22 @@
+namespace ChallengeApp.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenPointsAreAddedItShouldGiveTheSumOfThePoints()
+        {
+            //arrange
+            var employee2 = new Employee("Ada ", "Maze", 23);
+          
+            employee2.AddScore(5);
+            employee2.AddScore(-1);
+            employee2.AddScore(-2);
+            
+            // act
+            var result = employee2.Result;
+
+            //assert
+            Assert.AreEqual(2, result);
+        }
+    }
+}
