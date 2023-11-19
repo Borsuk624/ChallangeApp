e0de9b59f2893d734210e7126f0dd138b270d645
Łukasz Rodak <lukasz.rodak89@gmail.com>
2023-11-19 17:28:49 +0100

    dzień 10

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,11 +1,13 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Adam", "Maeve");
-employee.AddGrade(2);
-employee.AddGrade(2);
+employee.AddGrade("20");
+employee.AddGrade(4.5f);
+employee.AddGrade(2.66d);
 employee.AddGrade(6);
 var statistics = employee.GetStatistics();
-Console.WriteLine($"Average: { statistics.Average:N2}");
+
+Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
 
--- ChallengeApp/ChallengeApp/employee.cs
+++ ChallengeApp/ChallengeApp/employee.cs
@@ -15,18 +15,61 @@
         public string Surname { get; private set; }
 
         public void AddGrade(float grade)
-        { 
-            this.grades.Add(grade); 
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("Sting is not float");
+            }
         }
 
-        public Statistics GetStatistics() 
+        //Praca domowa
+        public void AddGrade(char grade)
+        {
+            if (char.IsDigit(grade))
+            {
+                AddGrade((float)char.GetNumericValue(grade));
+            }
+            else
+            {
+                Console.WriteLine("Char is not float");
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            var doubleAsFloat = (float)grade;
+            AddGrade(doubleAsFloat);
+        }
+        public void AddGrade(long grade)
+        {
+            var longAsFloat = (float)grade;
+            AddGrade(longAsFloat);
+        }
+
+        // koniec pracy domowej
+
+        public Statistics GetStatistics()
         {
             var statistics = new Statistics();
             statistics.Average = 0;
             statistics.Max = float.MinValue;
             statistics.Min = float.MaxValue;
 
-            foreach(var grade in this.grades)
+            foreach (var grade in this.grades)
             {
                 statistics.Max = Math.Max(statistics.Max, grade);
                 statistics.Min = Math.Min(statistics.Min, grade);
